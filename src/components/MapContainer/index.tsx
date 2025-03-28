654194f6ab3d680c31f8a236643820380b846bef
Cho Zi Hao <j0el_93@hotmail.com>
2025-03-29 02:53:30 +0800

    Update Yamanote Line display

--- src/components/MapContainer/index.tsx
+++ src/components/MapContainer/index.tsx
@@ -10,14 +10,149 @@ export default function MapContainer() {
       height="calc(100vh - 50px)"
       // viewBox="0 0 200 200"
     >
+      <ArcConnector alignment="bottom-right" startX={640} startY={740} />
+      <LineIcon
+        startX={600}
+        startY={720}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="30"
+        title="Yūrakuchō"
+      />
+      <Connector alignment="horizontal" startX={580} startY={740} />
+      <LineIcon
+        startX={540}
+        startY={720}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="29"
+        title="Shimbashi"
+      />
+      <Connector alignment="horizontal" startX={520} startY={740} />
+      <LineIcon
+        startX={480}
+        startY={720}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="28"
+        title="Hamamatsuchō"
+      />
+      <Connector alignment="horizontal" startX={460} startY={740} />
+      <LineIcon
+        startX={420}
+        startY={720}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="27"
+        title="Tamachi"
+      />
+      <Connector alignment="horizontal" startX={400} startY={740} />
+      <LineIcon
+        startX={360}
+        startY={720}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="26"
+        title="Takanawa Gateway"
+      />
+      <Connector alignment="horizontal" startX={340} startY={740} />
+      <LineIcon
+        startX={300}
+        startY={720}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="25"
+        title="Shinagawa"
+      />
+      <ArcConnector alignment="bottom-left" startX={250} startY={690} />
+      <LineIcon
+        startX={230}
+        startY={650}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="24"
+        title="Ōsaki"
+      />
+      <Connector alignment="vertical" startX={250} startY={630} />
+      <LineIcon
+        startX={230}
+        startY={590}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="23"
+        title="Gotanda"
+      />
+      <Connector alignment="vertical" startX={250} startY={570} />
+      <LineIcon
+        startX={230}
+        startY={530}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="22"
+        title="Meguro"
+      />
+      <Connector alignment="vertical" startX={250} startY={510} />
+      <LineIcon
+        startX={230}
+        startY={470}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="21"
+        title="Ebisu"
+      />
+      <Connector alignment="vertical" startX={250} startY={450} />
+      <LineIcon
+        startX={230}
+        startY={410}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="20"
+        title="Shibuya"
+      />
+      <Connector alignment="vertical" startX={250} startY={390} />
+      <LineIcon
+        startX={230}
+        startY={350}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="19"
+        title="Harajuku"
+      />
+      <Connector alignment="vertical" startX={250} startY={330} />
+      <LineIcon
+        startX={230}
+        startY={290}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="18"
+        title="Yoyogi"
+      />
+      <Connector alignment="vertical" startX={250} startY={270} />
+      <LineIcon
+        startX={230}
+        startY={230}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="17"
+        title="Shinjuku"
+      />
+      <Connector alignment="vertical" startX={250} startY={210} />
+      <LineIcon
+        startX={230}
+        startY={170}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="16"
+        title="Shin-Ōkubo"
+      />
       <ArcConnector alignment="top-left" startX={300} startY={120} />
       <LineIcon
         startX={300}
         startY={100}
         strokeColor="#85B82D"
         linePrefix="JY"
-        stationNumber="10"
-        title="Komagome"
+        stationNumber="15"
+        title="Takadanobaba"
       />
       <Connector alignment="horizontal" startX={340} startY={120} />
       <LineIcon
@@ -25,8 +160,8 @@ export default function MapContainer() {
         startY={100}
         strokeColor="#85B82D"
         linePrefix="JY"
-        stationNumber="09"
-        title="Tabata"
+        stationNumber="14"
+        title="Mejiro"
       />
       <Connector alignment="horizontal" startX={400} startY={120} />
       <LineIcon
@@ -34,8 +169,8 @@ export default function MapContainer() {
         startY={100}
         strokeColor="#85B82D"
         linePrefix="JY"
-        stationNumber="08"
-        title="Nishi-Nippori"
+        stationNumber="13"
+        title="Ikebukuro"
       />
       <Connector alignment="horizontal" startX={460} startY={120} />
 
@@ -44,8 +179,8 @@ export default function MapContainer() {
         startY={100}
         strokeColor="#85B82D"
         linePrefix="JY"
-        stationNumber="07"
-        title="Nippori"
+        stationNumber="12"
+        title="Ōtsuka"
       />
       <Connector alignment="horizontal" startX={520} startY={120} />
 
@@ -54,8 +189,8 @@ export default function MapContainer() {
         startY={100}
         strokeColor="#85B82D"
         linePrefix="JY"
-        stationNumber="06"
-        title="Uguisudani"
+        stationNumber="11"
+        title="Sugamo"
       />
       <Connector alignment="horizontal" startX={580} startY={120} />
       <LineIcon
@@ -63,10 +198,90 @@ export default function MapContainer() {
         startY={100}
         strokeColor="#85B82D"
         linePrefix="JY"
+        stationNumber="10"
+        title="Komagome"
+      />
+      <ArcConnector alignment="top-right" startX={640} startY={120} />
+      <LineIcon
+        startX={670}
+        startY={170}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="09"
+        title="Tabata"
+      />
+      <Connector alignment="vertical" startX={690} startY={210} />
+      <LineIcon
+        startX={670}
+        startY={230}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="08"
+        title="Nishi-Nippori"
+      />
+      <Connector alignment="vertical" startX={690} startY={270} />
+      <LineIcon
+        startX={670}
+        startY={290}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="07"
+        title="Nippori"
+      />
+      <Connector alignment="vertical" startX={690} startY={330} />
+      <LineIcon
+        startX={670}
+        startY={350}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="06"
+        title="Uguisudani"
+      />
+      <Connector alignment="vertical" startX={690} startY={390} />
+      <LineIcon
+        startX={670}
+        startY={410}
+        strokeColor="#85B82D"
+        linePrefix="JY"
         stationNumber="05"
         title="Ueno"
       />
-      <ArcConnector alignment="top-right" startX={640} startY={120} />
+      <Connector alignment="vertical" startX={690} startY={450} />
+      <LineIcon
+        startX={670}
+        startY={470}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="04"
+        title="Okachimachi"
+      />
+      <Connector alignment="vertical" startX={690} startY={510} />
+      <LineIcon
+        startX={670}
+        startY={530}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="03"
+        title="Akihabara"
+      />
+      <Connector alignment="vertical" startX={690} startY={570} />
+      <LineIcon
+        startX={670}
+        startY={590}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="02"
+        title="Kanda"
+      />
+      <Connector alignment="vertical" startX={690} startY={630} />
+      <LineIcon
+        startX={670}
+        startY={650}
+        strokeColor="#85B82D"
+        linePrefix="JY"
+        stationNumber="01"
+        title="Tokyo"
+      />
     </svg>
   );
 }
